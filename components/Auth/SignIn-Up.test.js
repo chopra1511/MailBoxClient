78a6ccadbb52aed9b7c6d1de6022db59ac84a0bc
Rahul Chopra <scorpio.sk98@gmail.com>
2022-02-12 19:41:21 +0530

    Login Screen TDD

--- components/Auth/SignIn-Up.test.js
+++ components/Auth/SignIn-Up.test.js
@@ -31,3 +31,33 @@ describe("Auth Component", () => {
        expect(check).toBeInTheDocument();
      });
 });
+
+describe("Auth Component test2", () => {
+  test("render name Testing", () => {
+    render(<Auth />);
+    const check = screen.getByText("Password", { exact: false });
+    expect(check).toBeInTheDocument();
+  });
+
+  test("render nav", () => {
+    render(<Auth />);
+    const check = screen.getByText("HOME");
+    expect(check).toBeInTheDocument();
+  });
+
+  test("renders create new acc if the user has not clicked signup Testing", () => {
+    render(<Auth />);
+    const check = screen.getByText("PRODUCT", { exact: false });
+    expect(check).toBeInTheDocument();
+  });
+
+  test("renders LOGIN if the user has clicked Login with existing account Testing", () => {
+    render(<Auth />);
+
+    const buttonCheck = screen.getByText("LOGIN");
+    userEvent.click(buttonCheck);
+
+    const check = screen.getByText("HOME", { exact: false });
+    expect(check).toBeInTheDocument();
+  });
+});
